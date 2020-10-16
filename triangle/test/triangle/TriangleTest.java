c83883393c74325f19bac2739eecb347394ee65a
Rui Guedes <ruiguedes12@hotmail.com>
2020-10-16 19:12:02 +0100

    Finished coverage-based testing

--- triangle/test/triangle/TriangleTest.java
+++ triangle/test/triangle/TriangleTest.java
@@ -28,12 +28,30 @@ public class TriangleTest {
 
   @Test
   public void testTable() {
+    // Fully tests the first multi-conditional statement (line 20)
     testTriangle(0, 0, 0, INVALID);
     testTriangle(1, 0, 0, INVALID);
     testTriangle(1, 1, 0, INVALID);
+
+    // Fully tests the second multi-conditional statement (line 34)
     testTriangle(1, 2, 3, INVALID);
-    testTriangle(1, 1, 1, EQUILATERAL);
-    testTriangle(2, 2, 1, ISOSCELES);
+    testTriangle(3, 1, 2, INVALID);
+    testTriangle(2, 3, 1, INVALID);
+    
+    // Scalene Test
     testTriangle(2, 3, 4, SCALENE);
+
+    // Equilateral Test
+    testTriangle(1, 1, 1, EQUILATERAL);
+    
+    // Isosceles Test
+    testTriangle(2, 2, 1, ISOSCELES);
+    testTriangle(1, 2, 2, ISOSCELES);
+    testTriangle(2, 1, 2, ISOSCELES);
+
+    // Invalid isosceles triangle test
+    testTriangle(2, 2, 5, INVALID);
+    testTriangle(5, 2, 2, INVALID); 
+    testTriangle(2, 5, 2, INVALID);
   }
 }
