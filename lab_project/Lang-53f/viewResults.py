3d417aeae71b859e06f749a04e46ab06deaca832
Rui Guedes <ruiguedes22@gmail.com>
2020-11-18 16:54:47 +0000

    Added scrips that allow easily determine killed mutants

---
+++ lab_project/Lang-53f/viewResults.py
@@ -0,0 +1,35 @@
+import json
+
+###########
+## Usage ##
+###########
+
+# This script compares the specified file version with the one (results.json) present in judy's folder.
+
+fileVersion = "V0.json"
+
+# Get difference of two lists
+def diff(list1, list2):
+    return (list(list(set(list1)-set(list2)) + list(set(list2)-set(list1))))
+
+with open('./mutants_results/' + fileVersion) as file:
+    fileV0 = json.load(file)
+
+with open('../judy-3.0.0-M1/result.json') as file:
+    fileV1 = json.load(file)
+
+dataV0 = list(filter(lambda x: x["name"] == "org.apache.commons.lang.time.DateUtils", fileV0["classes"]))[0]
+dataV0Mutants = list(map(lambda x: x["operators"][0] + " | " + str(x["lines"][0]), list(filter(lambda x: x["lines"][0] >= 0, sorted(dataV0["notKilledMutant"], key=lambda mutant: mutant["lines"][0])))))
+
+dataV1 = list(filter(lambda x: x["name"] == "org.apache.commons.lang.time.DateUtils", fileV1["classes"]))[0]
+dataV1Mutants = list(map(lambda x: x["operators"][0] + " | " + str(x["lines"][0]), list(filter(lambda x: x["lines"][0] >= 0, sorted(dataV1["notKilledMutant"], key=lambda mutant: mutant["lines"][0])))))
+
+dataDiff = diff(dataV0Mutants, dataV1Mutants)
+
+print("Operator | Line")
+print("--------------------------------")
+
+for mutant in dataDiff:
+    print(mutant)
+    print("--------------------------------")
+
